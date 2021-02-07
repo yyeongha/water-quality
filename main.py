2f419e37fc6a1c2b078939caa252ce83b667b33a
ChoiJaeWon568 <ks11@kotech.co.kr>
2021-02-07 17:19:07 +0900

    yeongsangang gain and chl-a 0.50

--- main.py
+++ main.py
@@ -16,7 +16,17 @@ import os
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
-
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e) 
 
 
 # input parameter
